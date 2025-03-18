385fad586353aae6de89598fe770315b66529244
dae-ne <rafalcz29@gmail.com>
2025-03-18 17:19:38 +0100

    Move configuration out of the main function

--- source/main.cpp
+++ source/main.cpp
@@ -3,39 +3,38 @@
 #include "config.hpp"
 #include "game.hpp"
 
+Config config = {
+
+    // WINDOW //
+
+    "T-Rex Runner",     // title
+    600u,               // width
+    200u,               // height
+    10u,                // padding
+    60u,                // framerate limit
+
+
+    // COLORS //
+
+    8u,                 // bits per pixel
+
+
+    // GAME //
+
+    40.f,               // dinosaur X position
+    180.f,              // ground level (y position)
+    10.f,               // initial jump speed
+    .4f,                // gravitational acceleration
+
+
+    // PATHS //
+
+    "assets/font.ttf",  // font path
+    
+};
+
 int main()
 {
-    Config config = {
-
-        // WINDOW //
-
-        "T-Rex Runner",     // title
-        600u,               // width
-        200u,               // height
-        10u,                // padding
-        60u,                // framerate limit
-
-
-        // COLORS //
-
-        8u,                 // bits per pixel
-
-
-        // GAME //
-
-        40.f,               // dinosaur X position
-        180.f,              // ground level (y position)
-        10.f,               // initial jump speed
-        .4f,                // gravitational acceleration
-
-
-        // PATHS //
-
-        "assets/font.ttf",  // font path
-        
-    };
-    
-
     auto game = Game(config);
     auto [success, errorMessage] = game.loadFiles(config);
 
